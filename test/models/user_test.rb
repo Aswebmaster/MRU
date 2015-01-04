0e0df53d2285704125c853feb7695ebcab8029a8
Aswebmaster <adriansierociuk@gmail.com>
2015-01-05 00:54:00 +0100

    make

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -91,4 +91,33 @@ class UserTest < ActiveSupport::TestCase
       @user.destroy
     end
   end
+
+  test "should follow and unfollow a user" do
+    adrian = users(:adrian)
+    natalia  = users(:natalia)
+    assert_not adrian.following?(natalia)
+    michael.follow(natalia)
+    assert adrian.following?(natalia)
+    assert adrian.followers.include?(adrian)
+    adrian.unfollow(natalia)
+    assert_not adrian.following?(natalia)
+  end
+
+  test "feed should have the right posts" do
+    michael = users(:michael)
+    archer  = users(:archer)
+    lana    = users(:lana)
+    # Posts from followed user
+    lana.microposts.each do |post_following|
+      assert michael.feed.include?(post_following)
+    end
+    # Posts from self
+    michael.microposts.each do |post_self|
+      assert michael.feed.include?(post_self)
+    end
+    # Posts from unfollowed user
+    archer.microposts.each do |post_unfollowed|
+      assert_not michael.feed.include?(post_unfollowed)
+    end
+  end
 end
